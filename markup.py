a999574b8ade3f241a7659f953834b83ba003bff
Rabdan <rabdan75@gmail.com>
2023-06-09 19:53:45 +0700

    add payment

--- markup.py
+++ markup.py
@@ -16,6 +16,7 @@ mainDMD.add(btDMD, btProfile, btBuy)
 sub_inline_murk = InlineKeyboardMarkup(row_width=2)
 
 
-btnSubMoth = InlineKeyboardButton(text="Месяц - 199 рублей", callback_data='submonth')
-btnSubMoth3 = InlineKeyboardButton(text="3 Месяца - 477 рублей", callback_data='submonth3')
+btnSubMoth = InlineKeyboardButton(text="Месяц - 199 рублей", callback_data='submonth_1')
+btnSubMoth3 = InlineKeyboardButton(text="3 Месяца - 477 рублей", callback_data='submonth_3')
 sub_inline_murk.add(btnSubMoth)
+sub_inline_murk.add(btnSubMoth3)
